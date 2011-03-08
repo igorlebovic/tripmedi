c9671ea220f427a157db7ed9714bff63110de3c9
Igor Lebovic <igor.lebovic@gmail.com>
2011-03-08 17:46:38 -0500

    added relative path

--- app/views.py
+++ app/views.py
@@ -1,5 +1,11 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse('blah')
+    return render_to_response('index.html')
+    # return HttpResponse('blah')
+
+def people(request):
+    return HttpResponse('people')
+
