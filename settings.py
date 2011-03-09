dcb85e16559c264168994b8e8aaa3791d78852bd
Ravi <ravi@iujheroinjgso.(none)>
2011-03-09 20:17:39 +0000

    social disable

--- settings.py
+++ settings.py
@@ -95,5 +95,5 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'tripmedi.app',
-    'socialauth',
+    #'socialauth',
 )
--- urls.py
+++ urls.py
@@ -7,6 +7,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^', include('tripmedi.app.urls')),
-    (r'^', include('tripmedi.socialauth.urls')),
+    #(r'^', include('tripmedi.socialauth.urls')),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
