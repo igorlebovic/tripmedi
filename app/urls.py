d70dccffd9d3bcedb1116b9ebc9c76f631a20cb8
Ravi <ravi@iujheroinjgso.(none)>
2011-03-09 23:12:53 +0000

    template rejig

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('tripmedi.app.views',
       (r'^$', 'index'),
+      (r'^listing/(?P<procedure_name>[-\w]+)/$', 'listing'),
+      (r'^procedure/(?P<procedure_name>[-\w]+)/$', 'procedure'),
       (r'^search$', 'search'),
-      (r'^procedure/(?P<procedure_name>[-\w]+)/$', 'procedure')
+      (r'^about$', 'about'),
+
 )
