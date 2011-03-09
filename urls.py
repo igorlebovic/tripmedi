a9411ed4b2de6b735d95195ba78292996922a495
Igor Lebovic <igor.lebovic@gmail.com>
2011-03-09 01:18:16 -0500

    tuple fix

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -7,4 +8,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^', include('tripmedi.app.urls')),
     (r'^admin/', include(admin.site.urls)),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
