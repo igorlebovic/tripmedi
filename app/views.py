55958283b600cbf0b2ad44a8013402b01f382d73
Ravi <ravi@iujheroinjgso.(none)>
2011-03-10 20:50:35 +0000

    make hoepage non default

--- app/views.py
+++ app/views.py
@@ -8,8 +8,13 @@ from django.core import urlresolvers
 import app.models
 import itertools
 
-def index(request, template_name='index.html'):
+def index2(request, template_name='index.html'):
 
+    category_list = app.models.MedicalCategory.objects.all()
+    return render_to_response(template_name, locals(), context_instance=RequestContext(request))
+    # return HttpResponse('blah')
+
+def index(request, template_name='index.html'):
     category_list = app.models.MedicalCategory.objects.all()
     return render_to_response(template_name, locals(), context_instance=RequestContext(request))
     # return HttpResponse('blah')
