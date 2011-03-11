363eae220b6e462eebca59f9da7936cec6a8a7e2
Ravi <ravi@iujheroinjgso.(none)>
2011-03-11 05:21:57 +0000

    procedure template working

--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ from tripmedi import settings
 from django.core import urlresolvers
 import app.models
 import itertools
+import random
 
 def index2(request, template_name='index2.html'):
 
@@ -20,6 +21,13 @@ def index(request, template_name='index.html'):
     # return HttpResponse('blah')
 
 def procedure(request, procedure_name, template_name="procedure.html"):
+    selected_procedure = app.models.Procedure.objects.get(pk=int(procedure_name))
+
+
+    poss_providers = selected_procedure.providerprocedure_set.order_by('procedure_rank').all()[:5]
+
+    all_countries_count = poss_providers.count()
+    random_country_list = ', '.join([str(x.provider.country.country_name) for x in poss_providers[:3]])
     return render_to_response(template_name, locals(), context_instance=RequestContext(request))
 
 def selection(request, procedure_name, provider_name, template_name="selection.html"):
