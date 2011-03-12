d2d39bddcb37b5a57a8c378f9939c3dc973400a2
Ravi <ravi@iujheroinjgso.(none)>
2011-03-12 23:38:04 +0000

    adding disqus

--- settings.py
+++ settings.py
@@ -91,7 +91,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.webdesign',
-    'django.templatetags.humanize',
+    'django.contrib.humanize',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
